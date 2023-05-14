12b50685a7ea4821e8105ac71c895241407c3a48
nuwan <techiprimers@gmail.com>
2023-05-14 17:32:46 +0530

    first connit

---
+++ src/app/EmployeeNew.ts
@@ -0,0 +1,16 @@
+
+export class Employee {
+
+  id : number;
+  employeeName : string:
+  mobileNo : string;
+  departmentNew : DepartmentNew;
+
+
+}
+
+
+export  class  DepartmentNew {
+  departmentId : number;
+  departmentName : string;
+}
